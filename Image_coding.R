af5dc7260b8288275ebd2a0a17c5bce283f5c14f
Radzmi Shah <152348714+radzmi@users.noreply.github.com>
2024-06-10 13:07:54 +0800

    Add files via upload

---
+++ Image_coding.R
@@ -0,0 +1,54 @@
+# Load all library required
+library(imager)
+
+# Load Image
+# Flower Image
+flower1 <- load.image("/Users/PC 8/Documents/Image/flower1.jpg")
+plot(flower1)
+
+flower2 <- load.image("/Users/PC 8/Documents/Image/flower2.jpg")
+plot(flower2)
+
+# Scenery Image
+scenery1 <- load.image("/Users/PC 8/Documents/Image/scenery1.jpg")
+plot(scenery1)
+
+scenery2 <- load.image("/Users/PC 8/Documents/Image/scenery2.jpg")
+plot(scenery2)
+
+# Person Image
+scenery1 <- load.image("/Users/PC 8/Documents/Image/scenery1.jpg")
+plot(scenery1)
+
+scenery2 <- load.image("/Users/PC 8/Documents/Image/scenery2.jpg")
+plot(scenery2)
+
+# Building Image
+building1 <- load.image("/Users/PC 8/Documents/Image/building1.jpg")
+plot(building1)
+
+building2 <- load.image("/Users/PC 8/Documents/Image/building2.jpg")
+plot(building2)
+
+# Other Image
+cat1 <- load.image("/Users/PC 8/Documents/Image/cat1.jpg")
+plot(cat1)
+
+cat2 <- load.image("/Users/PC 8/Documents/Image/cat2.jpg")
+plot(cat2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
